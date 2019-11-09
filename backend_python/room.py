da6b4f15b45e1ceeaf1b1b64dba3719894bd1262
Jutus Hache <justushache@yahoo.com>
2019-11-09 19:30:22 +0100

    worked on room.py

--- backend_python/room.py
+++ backend_python/room.py
@@ -5,3 +5,26 @@ class Room:
     timer = 90
     phase = 0
     running = False
+    
+    def __init___(self, id):
+        self.id = id
+            
+    
+    def addUser(self, user):
+        self.users.push(user)
+        for u in self.users:
+            #allert the u
+    
+    def changePhase(self,phase):
+        self.phase = phase
+        switch(phase){
+        case 0:break#schreibphase
+        case 1:break#kartenphase
+        case 2:break#aktionsphase
+        }
+        
+    def sendMessages(self,Message):
+        for u in users
+            #alltert the u
+       
+    

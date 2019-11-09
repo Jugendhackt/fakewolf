c8f590550be14dd02982881c00b5e255f886aac5
JanMoritz <jan@janmoritzmeyer.de>
2019-11-09 12:42:34 +0100

    createRoom

--- backend/index.php
+++ backend/index.php
@@ -2,7 +2,11 @@
 require_once  'settings.php';
 $file = str_replace("/","", @parse_url($_SERVER['REQUEST_URI'])['path']);
 switch (@parse_url($_SERVER['REQUEST_URI'])['path']) {
-    case '/':
+    case '/createRoom':
+        require 'createRoom.php';
+        break;
+    case '/sendMessage':
+        require 'createRoom.php';
         break;
     default:
         if(file_exists($file)){
